19a09a993b21ceeab04da4df7781880ce6f5d795
Sriram Kashyap <kssriramkashyap002@gmail.com>
2023-12-16 20:15:25 +0530

    got to know aboout the widget types

--- lib/Activity/home.dart
+++ lib/Activity/home.dart
@@ -8,10 +8,50 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+
+  void timer(){
+    //process = 5 sec
+    Future.delayed(Duration(seconds: 3),(){
+      print("alaram is riniging");
+    });
+    print("You can do Other Stuff Tooo");
+  }
+
+  int counter = 1;
   @override
+  void initState() {
+    super.initState();
+    timer();
+    print("init state");
+  }
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    print("Set State Called");
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    print("Widget Destroyed");
+  }
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Activity"),
+      appBar: AppBar(
+        title: Text("HOME ACTIVITY"),
+      ),
+      body:
+      Column(
+        children:<Widget> [
+          FloatingActionButton(
+            onPressed: () => setState(() {
+              counter += 1;
+            }),
+          ),
+          Text("$counter"),
+        ],
+      )
+
     );
   }
 }
